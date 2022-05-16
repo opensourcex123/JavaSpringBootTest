13f9ed31f61dcb5a792d3677df54b248cf2710dc
nfx.git <13561592791@163.com>
2022-05-16 09:57:27 +0800

    新增退款成功接口

--- src/main/java/com/nfx/springbootdemo02/server/service/impl/ReturnRequestServiceImpl.java
+++ src/main/java/com/nfx/springbootdemo02/server/service/impl/ReturnRequestServiceImpl.java
@@ -1,7 +1,9 @@
 package com.nfx.springbootdemo02.server.service.impl;
 
 import com.nfx.springbootdemo02.dao.model.ReturnRequest;
+import com.nfx.springbootdemo02.dao.model.User;
 import com.nfx.springbootdemo02.dao.repo.ReturnRequestDao;
+import com.nfx.springbootdemo02.dao.repo.UserDao;
 import com.nfx.springbootdemo02.server.service.IReturnRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import java.util.List;
 public class ReturnRequestServiceImpl implements IReturnRequestService {
     @Autowired
     private ReturnRequestDao returnRequestDao;
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public List<ReturnRequest> getReturnRequestByBusiness(ReturnRequest returnRequest) {
@@ -20,6 +24,52 @@ public class ReturnRequestServiceImpl implements IReturnRequestService {
 
     @Override
     public int updateReturnRequestById(ReturnRequest returnRequest) {
-        return returnRequestDao.updateDataById(returnRequest.getId(), returnRequest.getStatus(), returnRequest.getRejectMessage());
+        ReturnRequest returnRequestTable = returnRequestDao.getById(returnRequest.getId());
+        if (returnRequest.getStatus() == 1) {
+            String userName = returnRequestTable.getUserName();
+            String businessMan = returnRequestTable.getBusinessMan();
+            User user = userDao.getDataByName(userName);
+            User business = userDao.getDataByName(businessMan);
+            User admin = userDao.getDataByName("admin");
+            // 计算管理员和卖家应退的钱
+            int money = returnRequestTable.getMoney();
+            int adminMoney = 0;
+            int payeeMoney;
+            switch (business.getBusinessLevel()) {
+                case 1 -> {
+                    adminMoney = (int) (money * 0.001);
+                    payeeMoney = money - adminMoney;
+                }
+                case 2 -> {
+                    adminMoney = (int) (money * 0.002);
+                    payeeMoney = money - adminMoney;
+                }
+                case 3 -> {
+                    adminMoney = (int) (money * 0.005);
+                    payeeMoney = money - adminMoney;
+                }
+                case 4 -> {
+                    adminMoney = (int) (money * 0.0075);
+                    payeeMoney = money - adminMoney;
+                }
+                case 5 -> {
+                    adminMoney = (int) (money * 0.01);
+                    payeeMoney = money - adminMoney;
+                }
+                default -> payeeMoney = money;
+            }
+            // 给用户退钱
+            int preIncome = user.getIncome();
+            userDao.updateIncomeByName(userName, preIncome + returnRequestTable.getMoney());
+            // 把卖家的钱收回来
+            preIncome = business.getIncome();
+            userDao.updateIncomeByName(businessMan, preIncome - payeeMoney);
+            // 把admin的钱收回来
+            preIncome = admin.getIncome();
+            userDao.updateIncomeByName("admin", preIncome - adminMoney);
+            return returnRequestDao.updateDataById(returnRequest.getId(), returnRequest.getStatus(), returnRequest.getRejectMessage());
+        } else {
+            return returnRequestDao.updateDataById(returnRequest.getId(), returnRequest.getStatus(), returnRequest.getRejectMessage());
+        }
     }
 }
